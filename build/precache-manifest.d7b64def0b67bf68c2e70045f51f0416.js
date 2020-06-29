686ed5dd7639908b9a6c1d3a621ab7d09736672b
Tumaho <obadatumah19@gmail.com>
2020-06-29 16:54:01 +0300

    lab27

---
+++ build/precache-manifest.d7b64def0b67bf68c2e70045f51f0416.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e1bd46d1c6cb745e9659b725f017b8",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "96d71d233789df81c88a",
+    "url": "/deploy/static/css/main.dc46258e.chunk.css"
+  },
+  {
+    "revision": "fcdc19f9bcaff2ee8fc1",
+    "url": "/deploy/static/js/2.c33245e0.chunk.js"
+  },
+  {
+    "revision": "96d71d233789df81c88a",
+    "url": "/deploy/static/js/main.ec2fcb52.chunk.js"
+  },
+  {
+    "revision": "17423b9d7c2bb1f1de62",
+    "url": "/deploy/static/js/runtime~main.9c2cdec9.js"
+  }
+]);
